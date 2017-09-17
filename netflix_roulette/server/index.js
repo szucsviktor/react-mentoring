c8fcbaf27de25291bc9ef23b5e6737db62610235
szviktor <mexsherpa@gmail.com>
2017-09-17 21:37:22 +0200

    #Netflix roulette app: 'build' script has been changed to use 'webpack -p' in order to generate a production-like code decreasing the file size. Express static file path has been set. Index.html has been set to '/' path to be rendered from server-side.

--- netflix_roulette/server/index.js
+++ netflix_roulette/server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.get("*",(req, res) => {
-    res.send('Server is running');
+app.use(express.static(path.join(__dirname, "../")));
+
+app.get("/",(req, res) => {
+    res.sendFile(path.join(__dirname, "../","index.html"));
 });
 
 app.listen(3001);
